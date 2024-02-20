8ab8913d20fd74bc6e8b33596381e5a80747bab8
f-pistone <francescopistonedev@gmail.com>
2024-02-20 22:21:08 +0100

    ADD opening and closing of the options menu of the form

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-
+  //Button for opening the options menu of the form
+  $("#forms_list").on("click", ".open-options-menu", function () {
+    const options_menu_to_open = $(this).next(".options-menu");
+    if ($(options_menu_to_open).hasClass("hidden")) {
+      $(options_menu_to_open).removeClass("hidden");
+    } else {
+      $(options_menu_to_open).addClass("hidden");
+    }
+  });
 });
