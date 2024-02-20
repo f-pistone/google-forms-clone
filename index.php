787477f09ddf99aa988290953033e5df4614728b
f-pistone <francescopistonedev@gmail.com>
2024-02-20 14:10:59 +0100

    ADD Tailwind CSS

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" type="image/x-icon" href="./assets/images/favicon.ico">
+  <script src="https://cdn.tailwindcss.com"></script>
   <title>Google Forms Clone</title>
 </head>
 
