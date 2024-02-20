6296b57ed43760932fcf3322d38077b715c46660
f-pistone <francescopistonedev@gmail.com>
2024-02-20 16:08:57 +0100

    ADD head components

---
+++ components/head.php
@@ -0,0 +1,8 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/x-icon" href="./assets/images/favicon.ico">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="stylesheet" href="./app.css">
+  <title>Google Forms Clone</title>
+</head>
--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" type="image/x-icon" href="./assets/images/favicon.ico">
-  <script src="https://cdn.tailwindcss.com"></script>
-  <title>Google Forms Clone</title>
-</head>
+<?php include_once("./components/head.php") ?>
 
 <body>
 
