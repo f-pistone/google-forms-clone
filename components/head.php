3a46787e6c806aa12978aaf5ce4ce6bfe8a11071
f-pistone <francescopistonedev@gmail.com>
2024-02-20 17:11:21 +0100

    ADD jQuery to the head

--- components/head.php
+++ components/head.php
@@ -2,6 +2,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" type="image/x-icon" href="./assets/images/favicon.ico">
+  <script src="./jquery-3.7.1.min.js"></script>
   <script src="https://cdn.tailwindcss.com"></script>
   <link rel="stylesheet" href="./app.css">
   <title>Google Forms Clone</title>
