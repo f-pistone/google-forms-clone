6ca738d26681ceb2f478be7d3e8f8d8454aca536
f-pistone <francescopistonedev@gmail.com>
2024-02-21 18:01:32 +0100

    ADD Toastify JS

--- components/head.php
+++ components/head.php
@@ -4,6 +4,8 @@
   <link rel="icon" type="image/x-icon" href="./assets/images/favicon.ico">
   <script src="./jquery-3.7.1.min.js"></script>
   <script src="https://cdn.tailwindcss.com"></script>
+  <script src="./assets/toastify/toastify-js"></script>
+  <link rel="stylesheet" href="./assets/toastify/toastify.min.css">
   <link rel="stylesheet" href="./app.css">
   <title>Google Forms Clone</title>
 </head>
