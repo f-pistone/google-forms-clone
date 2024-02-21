8fb8dcc6eab84faa73e8c130d21b49ece5748ef0
f-pistone <francescopistonedev@gmail.com>
2024-02-21 19:00:46 +0100

    ADD redirection to the form edit page

--- index.php
+++ index.php
@@ -123,14 +123,14 @@ $id_user = (int)$_SESSION['id_user'];
                           </button>
                         </li>
                         <li class="hover:bg-slate-100">
-                          <div class="w-full p-3 flex items-center gap-3">
+                          <a class="w-full p-3 flex items-center gap-3" href="./edit_form.php?id_form=<?= $id_form ?>" target="_blank">
                             <span class="text-lg font-bold">
                               <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 2048 2048">
                                 <path fill="currentColor" d="M2048 0v1664h-384v384H0V384h384V0zm-128 1536V128H512v256h256v128H128v1408h1408v-640h128v256zm-979-339l-90-90l594-595h-421V384h640v640h-128V603z" />
                               </svg>
                             </span>
                             <span>Open in a new tab</span>
-                          </div>
+                          </a>
                         </li>
                       </ul>
                     </div>
