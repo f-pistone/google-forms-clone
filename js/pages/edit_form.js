fc16e9251febe140108ab2779558eadf6501e9fd
f-pistone <francescopistonedev@gmail.com>
2024-02-22 21:40:03 +0100

    ADD creation of a new question

--- js/pages/edit_form.js
+++ js/pages/edit_form.js
@@ -16,4 +16,92 @@ $(document).ready(function () {
     $(".form-box").removeClass("active-form-box");
     $(this).addClass("active-form-box");
   });
+
+  //Button to add a new question
+  $("#add_question_button").on("click", async function () {
+    const last_question = $(".question").last();
+    const new_question = await createQuestion();
+
+    if (last_question.length > 0) {
+      $(last_question).after(new_question);
+    } else {
+      $(".section").first().after(new_question);
+    }
+  });
 });
+
+//Get the question types in the database
+async function getQuestionTypes() {
+  const question_types = await $.ajax({
+    type: "GET",
+    url: "php/get_question_types.php",
+  });
+  return question_types;
+}
+
+//Create the options for the select with the question types for the question
+async function createOptionsWithQuestionTypes() {
+  let options = "";
+  const question_types = await getQuestionTypes();
+  for (let i = 0; i < question_types.length; i++) {
+    let id_question_type = question_types[i].id_question_type;
+    let name_question_type = question_types[i].name_question_type;
+    options += `<option value="${id_question_type}">${name_question_type}</option>`;
+  }
+  return options;
+}
+
+//Create a new question
+async function createQuestion() {
+  let options_with_question_types = await createOptionsWithQuestionTypes();
+  const new_question = `            
+  <!-- QUESTION SHORT ANSWER -->
+  <div class="question form-box relative p-7 rounded-md bg-white shadow flex flex-col gap-4">
+
+    <!-- QUESTION HEADER -->
+    <div class="question-header flex flex-wrap justify-start md:justify-between items-center gap-5">
+      <div class="basis-full md:basis-6/12">
+        <input type="text" class="p-4 w-full bg-gray-100 border-b border-gray-500 focus:outline-none focus:border-b-2 focus:border-violet-800 transition" placeholder="Question" value="">
+      </div>
+      <div class="basis-full md:basis-5/12">
+        <select class="p-4 w-full cursor-pointer border rounded focus:outline-none">
+          ${options_with_question_types}
+        </select>
+      </div>
+    </div>
+    <!-- END QUESTION HEADER -->
+
+    <!-- QUESTION BODY -->
+    <div class="question-body mb-10">
+      <p class="border-dotted border-b border-gray-500 text-gray-500">Short answer</p>
+    </div>
+    <!-- END QUESTION BODY -->
+
+    <!-- QUESTION FOOTER -->
+    <div class="question-footer pt-2 border-t border-gray-300 flex justify-end items-center gap-3">
+      <div class="pr-2 border-r border-gray-300 flex items-center gap-1">
+        <button type="button" class="p-3 w-[50px] aspect-square rounded-full flex justify-center items-center text-lg transition hover:bg-gray-100 focus:bg-gray-200">
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16">
+            <path fill="currentColor" fill-rule="evenodd" d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z" />
+          </svg>
+        </button>
+        <button type="button" class="p-3 w-[50px] aspect-square rounded-full flex justify-center items-center text-lg transition hover:bg-gray-100 focus:bg-gray-200">
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512">
+            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="m112 112l20 320c.95 18.49 14.4 32 32 32h184c17.67 0 30.87-13.51 32-32l20-320" />
+            <path fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="32" d="M80 112h352" />
+            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M192 112V72h0a23.93 23.93 0 0 1 24-24h80a23.93 23.93 0 0 1 24 24h0v40m-64 64v224m-72-224l8 224m136-224l-8 224" />
+          </svg>
+        </button>
+      </div>
+      <div class="pl-2 flex items-center gap-2">
+        <span>Required</span>
+        <input class="switch" type="checkbox">
+      </div>
+    </div>
+    <!-- END QUESTION FOOTER -->
+
+  </div>
+  <!-- END QUESTION SHORT ANSWER -->
+  `;
+  return new_question;
+}
