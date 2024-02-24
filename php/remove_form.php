4507e0c7f891300827c488aab04c15cdeae24151
f-pistone <francescopistonedev@gmail.com>
2024-02-24 18:56:33 +0100

    ADD removing of sections and questions when a form is removed

--- php/remove_form.php
+++ php/remove_form.php
@@ -3,6 +3,18 @@ include_once("../database/conn.php");
 
 $id_form = (int)$_POST['id_form'];
 
+$sqlGetFormSections = "SELECT id_section FROM sections WHERE id_form = $id_form";
+$queryGetFormSections = mysqli_query($conn, $sqlGetFormSections) or die("Error: get form's sections");
+
+while ($rowGetFormQuestions = mysqli_fetch_assoc($queryGetFormSections)) {
+  $id_section = (int)$rowGetFormQuestions['id_section'];
+  $sqlRemoveSectionQuestions = "DELETE FROM questions WHERE id_section = $id_section";
+  $queryRemoveSectionQuestions = mysqli_query($conn, $sqlRemoveSectionQuestions) or die("Error: remove form's section's questions");
+}
+
+$sqlRemoveFormSections = "DELETE FROM sections WHERE id_form = $id_form";
+$queryRemoveFormSections = mysqli_query($conn, $sqlRemoveFormSections) or die("Error: remove form's sections");
+
 $sqlRemoveForm = "DELETE FROM forms WHERE id_form = $id_form";
 $queryRemoveForm = mysqli_query($conn, $sqlRemoveForm) or die("Error: remove form");
 
