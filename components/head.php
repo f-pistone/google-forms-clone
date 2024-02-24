98ab1a302c6bf05507300378e7dd5b3baa25019c
f-pistone <francescopistonedev@gmail.com>
2024-02-24 10:39:32 +0100

    ADD Tailwind as JS file from the project

--- components/head.php
+++ components/head.php
@@ -3,7 +3,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" type="image/x-icon" href="./assets/images/favicon.ico">
   <script src="./jquery-3.7.1.min.js"></script>
-  <script src="https://cdn.tailwindcss.com"></script>
+  <script src="./assets/tailwind/tailwind.3.4.1.js"></script>
   <script src="./assets/toastify/toastify-js"></script>
   <link rel="stylesheet" href="./assets/toastify/toastify.min.css">
   <link rel="stylesheet" href="./app.css">
