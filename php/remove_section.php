3407ca65f8e560328dd6036b81c916d8305a2391
f-pistone <francescopistonedev@gmail.com>
2024-02-25 15:48:46 +0100

    FIX delete of the forms, the sections, the questions and the options

--- php/remove_section.php
+++ php/remove_section.php
@@ -3,6 +3,14 @@ include_once("../database/conn.php");
 
 $id_section = (int)$_POST['id_section'];
 
+$sqlGetSectionQuestions = "SELECT id_question FROM questions WHERE id_section = $id_section";
+$queryGetSectionQuestions = mysqli_query($conn, $sqlGetSectionQuestions) or die("Error: get section's questions");
+while ($rowGetSectionQuestions = mysqli_fetch_assoc($queryGetSectionQuestions)) {
+  $id_question = (int)$rowGetSectionQuestions['id_question'];
+  $sqlRemoveQuestionOptions = "DELETE FROM options WHERE id_question = $id_question";
+  $queryRemoveQuestionOptions = mysqli_query($conn, $sqlRemoveQuestionOptions) or die("Error: remove question's options");
+}
+
 $sqlRemoveSectionQuestions = "DELETE FROM questions WHERE id_section = $id_section";
 $queryRemoveSectionQuestions = mysqli_query($conn, $sqlRemoveSectionQuestions) or die("Error: remove section's questions");
 
