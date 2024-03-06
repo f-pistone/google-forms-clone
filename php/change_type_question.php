9a16184cfaef1a63595703f5a7946270ba1d5ef5
f-pistone <francescopistonedev@gmail.com>
2024-03-06 14:38:50 +0100

    FIX change type question

--- php/change_type_question.php
+++ php/change_type_question.php
@@ -7,7 +7,7 @@ $new_type_question = addslashes($_POST['new_type_question']);
 $sqlChangeTypeQuestion = "UPDATE 
                             questions 
                           SET 
-                            type_question = '$new_type_question', answer_question = '' 
+                            type_question = '$new_type_question' 
                           WHERE 
                             id_question = $id_question";
 $queryChangeTypeQuestion = mysqli_query($conn, $sqlChangeTypeQuestion) or die("Error: change type question");
