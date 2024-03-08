538a249224fb239deb1798e85a1ca7433f541b07
f-pistone <francescopistonedev@gmail.com>
2024-03-08 15:45:05 +0100

    ADD showing the edit form button only if the user owns the form

--- viewform.php
+++ viewform.php
@@ -1,18 +1,15 @@
 <?php
 include_once("./database/conn.php");
 session_start();
-if (empty($_SESSION['id_user'])) {
-  header("Location: login.php");
-}
 
-$id_user = (int)$_SESSION['id_user'];
+$user_owns_form = 0;
 $id_form = (int)$_GET['id_form'];
 
-$sqlCheckIfUserOwnsForm = "SELECT id_form, id_user FROM forms WHERE id_form = $id_form AND id_user = $id_user";
-$queryCheckIfUserOwnsForm = mysqli_query($conn, $sqlCheckIfUserOwnsForm) or die("Error: check if user owns form");
-$user_owns_form = (int)mysqli_num_rows($queryCheckIfUserOwnsForm);
-if ($user_owns_form === 0) {
-  header("Location: index.php");
+if (!empty($_SESSION['id_user'])) {
+  $id_user = (int)$_SESSION['id_user'];
+  $sqlCheckIfUserOwnsForm = "SELECT id_form, id_user FROM forms WHERE id_form = $id_form AND id_user = $id_user";
+  $queryCheckIfUserOwnsForm = mysqli_query($conn, $sqlCheckIfUserOwnsForm) or die("Error: check if user owns form");
+  $user_owns_form = (int)mysqli_num_rows($queryCheckIfUserOwnsForm);
 }
 
 //Form informations
@@ -373,16 +370,22 @@ while ($rowGetForm = mysqli_fetch_assoc($queryGetForm)) {
 
     </div>
 
-    <!-- EDIT BUTTON -->
-    <a href="./edit_form.php?id_form=<?= $id_form ?>" class="fixed right-5 bottom-5 z-[999] flex justify-center items-center bg-white text-violet-800 text-2xl w-[50px] aspect-square rounded-full shadow hover:bg-gray-100" title="Edit this form">
-      <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-        <g fill="none">
-          <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
-          <path fill="currentColor" d="M16.035 3.015a3 3 0 0 1 4.099-.135l.144.135l.707.707a3 3 0 0 1 .135 4.098l-.135.144L9.773 19.177a1.5 1.5 0 0 1-.562.354l-.162.047l-4.454 1.028a1.001 1.001 0 0 1-1.22-1.088l.02-.113l1.027-4.455a1.5 1.5 0 0 1 .29-.598l.111-.125zm-.707 3.535l-8.99 8.99l-.636 2.758l2.758-.637l8.99-8.99l-2.122-2.12Zm3.536-2.121a1 1 0 0 0-1.32-.083l-.094.083l-.708.707l2.122 2.121l.707-.707a1 1 0 0 0 .083-1.32l-.083-.094z" />
-        </g>
-      </svg>
-    </a>
-    <!-- END EDIT BUTTON -->
+    <?php
+    if ($user_owns_form == 1) {
+    ?>
+      <!-- EDIT BUTTON -->
+      <a href="./edit_form.php?id_form=<?= $id_form ?>" class="fixed right-5 bottom-5 z-[999] flex justify-center items-center bg-white text-violet-800 text-2xl w-[50px] aspect-square rounded-full shadow hover:bg-gray-100" title="Edit this form">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+          <g fill="none">
+            <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+            <path fill="currentColor" d="M16.035 3.015a3 3 0 0 1 4.099-.135l.144.135l.707.707a3 3 0 0 1 .135 4.098l-.135.144L9.773 19.177a1.5 1.5 0 0 1-.562.354l-.162.047l-4.454 1.028a1.001 1.001 0 0 1-1.22-1.088l.02-.113l1.027-4.455a1.5 1.5 0 0 1 .29-.598l.111-.125zm-.707 3.535l-8.99 8.99l-.636 2.758l2.758-.637l8.99-8.99l-2.122-2.12Zm3.536-2.121a1 1 0 0 0-1.32-.083l-.094.083l-.708.707l2.122 2.121l.707-.707a1 1 0 0 0 .083-1.32l-.083-.094z" />
+          </g>
+        </svg>
+      </a>
+      <!-- END EDIT BUTTON -->
+    <?php
+    }
+    ?>
 
   </main>
 
